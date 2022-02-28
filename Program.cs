05e5fb7469e00cb30e5f4350a6a344a8b484c47f
chibispy <victor.girelli@gmail.com>
2022-02-28 18:38:34 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using RPG.src.Entities;
+
+namespace RPG.Entities {
+    
+    class Program{
+    
+        static void Main(string[] args){
+
+            Hero Arus = new Hero("Arus", 24 , "knight");
+            Wizard Jenica = new Wizard("Jenica", 25, "red mage");
+
+            Console.WriteLine(Arus.Attack());
+            Console.WriteLine(Jenica);
+        }
+    }
+} 
+
---
+++ src/Entities/Wizard.cs
@@ -0,0 +1,18 @@
+namespace RPG.src.Entities
+{
+    public class Wizard : Hero
+    {
+
+
+        public Wizard(string Name, int Level, string HeroType) : base(Name, Level, HeroType)
+        {
+            this.Name = Name;
+            this.Level = Level;
+            this.HeroType = HeroType;
+        }
+        public override string Attack(){
+            return this.Name + " lancou magia \n"; 
+
+        }
+    }
+}
